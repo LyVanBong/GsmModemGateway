9cfbd9a95e50e726c52fe9108fe0401905ae9e78
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2023-03-31 22:13:52 +0700

    Thêm boot telegram

--- ApiGsm/GlobalUsings.cs
+++ ApiGsm/GlobalUsings.cs
@@ -3,3 +3,10 @@
 global using ApiGsm.Models;
 global using ApiGsm.Utils;
 global using Microsoft.AspNetCore.Mvc;
+global using Newtonsoft.Json;
+global using Telegram.Bot;
+global using Telegram.Bot.Exceptions;
+global using Telegram.Bot.Polling;
+global using Telegram.Bot.Types;
+global using Telegram.Bot.Types.Enums;
+global using Message = Telegram.Bot.Types.Message;
